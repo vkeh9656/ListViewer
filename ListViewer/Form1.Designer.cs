627d1dbd546c5f6c842ed6475868ba9ee58940df
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-06-07 00:26:33 +0900

    UI 세팅, PictureBox에 로직 구현

--- ListViewer/Form1.Designer.cs
+++ ListViewer/Form1.Designer.cs
@@ -28,13 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTotalCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgListView = new System.Windows.Forms.DataGridView();
+            this.pBox4 = new System.Windows.Forms.PictureBox();
+            this.pBox3 = new System.Windows.Forms.PictureBox();
+            this.pBox2 = new System.Windows.Forms.PictureBox();
+            this.pBox1 = new System.Windows.Forms.PictureBox();
+            this.lblPick1 = new System.Windows.Forms.Label();
+            this.lblPick2 = new System.Windows.Forms.Label();
+            this.lblPick3 = new System.Windows.Forms.Label();
+            this.lblPick4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgListView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "※ 선호하는 항목을 선택해주세요";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.lblPick4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPick3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPick2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPick1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pBox4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 41);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(432, 157);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lblTotalCount
+            // 
+            this.lblTotalCount.AutoSize = true;
+            this.lblTotalCount.Location = new System.Drawing.Point(12, 225);
+            this.lblTotalCount.Name = "lblTotalCount";
+            this.lblTotalCount.Size = new System.Drawing.Size(88, 12);
+            this.lblTotalCount.TabIndex = 2;
+            this.lblTotalCount.Text = "Total Count : 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "※ List의 Data를 화면에 보여줍니다.";
+            // 
+            // dgListView
+            // 
+            this.dgListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgListView.Location = new System.Drawing.Point(108, 302);
+            this.dgListView.Name = "dgListView";
+            this.dgListView.RowTemplate.Height = 23;
+            this.dgListView.Size = new System.Drawing.Size(240, 202);
+            this.dgListView.TabIndex = 4;
+            // 
+            // pBox4
+            // 
+            this.pBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBox4.Image = global::ListViewer.Properties.Resources._4;
+            this.pBox4.Location = new System.Drawing.Point(327, 3);
+            this.pBox4.Name = "pBox4";
+            this.pBox4.Size = new System.Drawing.Size(102, 126);
+            this.pBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox4.TabIndex = 3;
+            this.pBox4.TabStop = false;
+            this.pBox4.Click += new System.EventHandler(this.pBox_Click);
+            // 
+            // pBox3
+            // 
+            this.pBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBox3.Image = global::ListViewer.Properties.Resources._3;
+            this.pBox3.Location = new System.Drawing.Point(219, 3);
+            this.pBox3.Name = "pBox3";
+            this.pBox3.Size = new System.Drawing.Size(102, 126);
+            this.pBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox3.TabIndex = 2;
+            this.pBox3.TabStop = false;
+            this.pBox3.Click += new System.EventHandler(this.pBox_Click);
+            // 
+            // pBox2
+            // 
+            this.pBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBox2.Image = global::ListViewer.Properties.Resources._2;
+            this.pBox2.Location = new System.Drawing.Point(111, 3);
+            this.pBox2.Name = "pBox2";
+            this.pBox2.Size = new System.Drawing.Size(102, 126);
+            this.pBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox2.TabIndex = 1;
+            this.pBox2.TabStop = false;
+            this.pBox2.Click += new System.EventHandler(this.pBox_Click);
+            // 
+            // pBox1
+            // 
+            this.pBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBox1.Image = global::ListViewer.Properties.Resources._1;
+            this.pBox1.Location = new System.Drawing.Point(3, 3);
+            this.pBox1.Name = "pBox1";
+            this.pBox1.Size = new System.Drawing.Size(102, 126);
+            this.pBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBox1.TabIndex = 0;
+            this.pBox1.TabStop = false;
+            this.pBox1.Click += new System.EventHandler(this.pBox_Click);
+            // 
+            // lblPick1
+            // 
+            this.lblPick1.AutoSize = true;
+            this.lblPick1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblPick1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPick1.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPick1.Location = new System.Drawing.Point(3, 132);
+            this.lblPick1.Name = "lblPick1";
+            this.lblPick1.Size = new System.Drawing.Size(102, 25);
+            this.lblPick1.TabIndex = 5;
+            this.lblPick1.Text = "0";
+            this.lblPick1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPick2
+            // 
+            this.lblPick2.AutoSize = true;
+            this.lblPick2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblPick2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPick2.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPick2.Location = new System.Drawing.Point(111, 132);
+            this.lblPick2.Name = "lblPick2";
+            this.lblPick2.Size = new System.Drawing.Size(102, 25);
+            this.lblPick2.TabIndex = 6;
+            this.lblPick2.Text = "0";
+            this.lblPick2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPick3
+            // 
+            this.lblPick3.AutoSize = true;
+            this.lblPick3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblPick3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPick3.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPick3.Location = new System.Drawing.Point(219, 132);
+            this.lblPick3.Name = "lblPick3";
+            this.lblPick3.Size = new System.Drawing.Size(102, 25);
+            this.lblPick3.TabIndex = 7;
+            this.lblPick3.Text = "0";
+            this.lblPick3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPick4
+            // 
+            this.lblPick4.AutoSize = true;
+            this.lblPick4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblPick4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPick4.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPick4.Location = new System.Drawing.Point(327, 132);
+            this.lblPick4.Name = "lblPick4";
+            this.lblPick4.Size = new System.Drawing.Size(102, 25);
+            this.lblPick4.TabIndex = 8;
+            this.lblPick4.Text = "0";
+            this.lblPick4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(464, 516);
+            this.Controls.Add(this.dgListView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTotalCount);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgListView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pBox1;
+        private System.Windows.Forms.Label lblTotalCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgListView;
+        private System.Windows.Forms.PictureBox pBox4;
+        private System.Windows.Forms.PictureBox pBox3;
+        private System.Windows.Forms.PictureBox pBox2;
+        private System.Windows.Forms.Label lblPick1;
+        private System.Windows.Forms.Label lblPick4;
+        private System.Windows.Forms.Label lblPick3;
+        private System.Windows.Forms.Label lblPick2;
     }
 }
 
--- ListViewer/Form1.cs
+++ ListViewer/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,101 @@ namespace ListViewer
 {
     public partial class Form1 : Form
     {
+        List<string> _strList = new List<string>();
+
+        List<int> _intList = new List<int>();
+
+        ArrayList _arrayList = new ArrayList();
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pBox_Click(object sender, EventArgs e)
+        {
+            PictureBox pbox = sender as PictureBox;
+
+            string strSelectText = string.Empty;
+
+            switch(pbox.Name) 
+            {
+                case "pBox1":
+                    strSelectText = "cake";
+                    break;
+                case "pBox2":
+                    strSelectText = "burger";
+                    break;
+                case "pBox3":
+                    strSelectText = "pizza";
+                    break;
+                case "pBox4":
+                    strSelectText = "icecream";
+                    break;
+            }
+
+            _strList.Add(strSelectText);
+
+            UIDisplay();
+
+            DataGridViewDisplay();
+        }
+
+        
+
+        private void UIDisplay()
+        {
+            int iCake = 0;
+            int iBurger = 0;
+            int iPizza = 0;
+            int iIcecream = 0;
+
+            foreach (var item in _strList)
+            {
+                switch (item)
+                {
+                    case "cake":
+                        iCake++;
+                        break;
+                    case "burger":
+                        iBurger++;
+                        break;
+                    case "pizza":
+                        iPizza++;
+                        break;
+                    case "icecream":
+                        iIcecream++;
+                        break;
+                }
+            }
+
+            lblPick1.Text = iCake.ToString();
+            lblPick2.Text = iBurger.ToString();
+            lblPick3.Text = iPizza.ToString();
+            lblPick4.Text = iIcecream.ToString();
+
+            lblTotalCount.Text = "Total Count: " + _strList.Count.ToString();
+        }
+
+        private void DataGridViewDisplay()
+        {
+            //dgListView.Rows.Clear();
+
+
+            //foreach(string item in _strList)
+            //{
+            //    dgListView.Rows.Add(item);
+            //}
+
+            dgListView.DataSource = _strList.Select(x => new { value = x }).ToList();
+
+            foreach(DataGridViewRow row in dgListView.Rows)
+            {
+                row.HeaderCell.Value = row.Index.ToString();
+            }
+
+            dgListView.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
         }
     }
 }
